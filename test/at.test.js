3c256a35e06d23f31dd49e731a8c562dfcfab5ae
TheKaski <emil.kaskikallio@gmail.com>
2023-11-08 11:06:07 +0200

    Endlessly testing the coveralls github actions integration

--- test/at.test.js
+++ test/at.test.js
@@ -5,7 +5,7 @@ import { should } from 'chai';
 import "mocha";
 
 const object = { 'a': [{ 'b': { 'c': 3 } }, 4] }
-
+// THis test is copied from the example in the file at.js
 describe('Testing At from Software-testing-assignment', () => {
   it("at.js ", () => {
     expect(at(object, ['a[0].b.c', 'a[1]'])).to.deep.equal([3, 4]);
