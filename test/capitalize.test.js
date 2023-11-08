082a642fa350e3f14a3d58b50d959f93a1ee7b61
TheKaski <emil.kaskikallio@gmail.com>
2023-11-08 11:57:47 +0200

    extra step plus planned test for capitalize

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -8,3 +8,30 @@ describe('', () => {
   it("", () => {
  });
 });
+
+
+
+/*PLANNING THE TEST CASES FOR THIS TEST:
+
+input: "string" -> Converted to String -> Converted to Form Xxxx...
+From any forms:
+
+Possible test cases:
+input = XXXX ALL CAPS
+input= Xxxx   Correct form 
+input = xxxX  Wrong form
+
+If input is/has a number(or specialcase character)(Number cannot be set to lower):
+input = 1111 
+input = 1abcd
+input = abcd1
+input = !abcd
+input = abcd!
+
+
+Equivalence classes:
+
+
+
+
+*/
