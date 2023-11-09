0cbbe3cce6614da0bb7e8cfe5c1d04683432d6e3
TheKaski <emil.kaskikallio@gmail.com>
2023-11-09 18:05:23 +0200

    Added .DS_Store to .gitignore

--- test/at.test.js
+++ test/at.test.js
@@ -1,25 +1,25 @@
 import {default as at} from "../src/at.js";
-import { assert } from 'chai';
-import { expect } from 'chai';
-import { should } from 'chai';
+import { assert } from "chai";
+import { expect } from "chai";
+import { should } from "chai";
 import "mocha";
 
-const object = { 'a': [{ 'b': { 'c': 3 } }, 4, null] }
+const object = { "a": [{ "b": { "c": 3 } }, 4, null] }
 // THis test is copied from the example in the file at.js
-describe('Testing At from Software-testing-assignment', () => {
+describe("Testing At from Software-testing-assignment", () => {
 
   describe("Correct values should be found", () => {
     it("Should return the correct value in array", () => {
-      expect(at(object, ['a[0].b.c'])).to.deep.equal([3]);
+      expect(at(object, ["a[0].b.c"])).to.deep.equal([3]);
     });
     it("Should return the null value in array ", () => {
-      expect(at(object, ['a[2]'])).to.deep.equal([null]);
+      expect(at(object, ["a[2]"])).to.deep.equal([null]);
     });
     it("Should return the value object in array", () => {
-      expect(at(object, ['a[0]'])).to.deep.equal([{ 'b': { 'c': 3 } }]);
+      expect(at(object, ["a[0]"])).to.deep.equal([{ "b": { "c": 3 } }]);
     });
     it("Should return the value array in array", () => {
-      expect(at(object, ['a'])).to.deep.equal([[{ 'b': { 'c': 3 } }, 4, null]]);
+      expect(at(object, ["a"])).to.deep.equal([[{ "b": { "c": 3 } }, 4, null]]);
     });
     it("Should return with empty list when path is empty", () => {
       expect(at(object, [])).to.deep.equal([]);
@@ -27,13 +27,13 @@ describe('Testing At from Software-testing-assignment', () => {
   });
   describe("Values cannot be found but return should be handled correctly", () => {
     it("Should return undefined when the value is not found", () => {
-      expect(at(object, ['a[0].ö'])).to.deep.equal([undefined]);
+      expect(at(object, ["a[0].ö"])).to.deep.equal([undefined]);
     });
     it("Should return undefined when the value index is out of bounds", () => {
-      expect(at(object, ['a[3]'])).to.deep.equal([undefined]);
+      expect(at(object, ["a[3]"])).to.deep.equal([undefined]);
     });
     it("Should return undefined when the value is not found", () => {
-      expect(at(object, ['a[0].ö'])).to.deep.equal([undefined]);
+      expect(at(object, ["a[0].ö"])).to.deep.equal([undefined]);
     });
    
     it("Should respond with undefined in array", () => {
@@ -49,19 +49,19 @@ describe('Testing At from Software-testing-assignment', () => {
 
   describe("Call with multiple valid paths", () => {
     it("Should respond with  the correct values in array ", () => {
-      expect(at(object, ['a[0].b.c', 'a[1]', 'a'])).to.deep.equal([3, 4, [{ 'b': { 'c': 3 } }, 4, null]]);
+      expect(at(object, ["a[0].b.c", "a[1]", "a"])).to.deep.equal([3, 4, [{ "b": { "c": 3 } }, 4, null]]);
     });
   });
 
   describe("Calls with unsearchable objects", () => {
     it("Should return with array of undefined values when object is undefined", () => {
-      expect(at(undefined, ['a[0].b.c', 'a[1]', 'a'])).to.deep.equal([undefined, undefined, undefined]);
+      expect(at(undefined, ["a[0].b.c", "a[1]", "a"])).to.deep.equal([undefined, undefined, undefined]);
     });
     it("Should return with array of undefined values when object is null", () => {
-      expect(at(null, ['a[0].b.c', 'a[1]', 'a'])).to.deep.equal([undefined, undefined, undefined]);
+      expect(at(null, ["a[0].b.c", "a[1]", "a"])).to.deep.equal([undefined, undefined, undefined]);
     });
     it("Should return with array of undefined values when object is empty", () => {
-      expect(at({}, ['a[0].b.c', 'a[1]', 'a'])).to.deep.equal([undefined, undefined, undefined]);
+      expect(at({}, ["a[0].b.c", "a[1]", "a"])).to.deep.equal([undefined, undefined, undefined]);
     });
     it("Should return with empty array when object is undefined and path is empty", () => {
       expect(at(undefined, [])).to.deep.equal([]);
