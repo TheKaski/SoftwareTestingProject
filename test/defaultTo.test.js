59f5fc589d96e614bfeb4ad2dc878d413de037a2
TheKaski <emil.kaskikallio@gmail.com>
2023-11-09 19:05:04 +0200

    Basic defaultTo tests written and one bug found

--- test/defaultTo.test.js
+++ test/defaultTo.test.js
@@ -4,8 +4,44 @@ import { expect } from 'chai';
 import { should } from 'chai';
 
 
-describe('', () => {
-  it("", () => {
-  
- });
+describe("DefaultTo", () => {
+  describe("Testing with undefined null and Nan values", () => {
+    it("Should return the default value when value is undefined", () => {
+      expect(defaultTo(undefined, "default")).to.equal("default");
+    });
+    
+    it("Should return the default value when value is null", () => {
+      expect(defaultTo(null, "default")).to.equal("default");
+    });
+    
+    it("Should return the default value when value is undefined", () => {
+      expect(defaultTo(NaN, "default")).to.equal("default");
+    });
+  });
+
+  describe("Testing with defined values", () => {
+    it("Should return the value when string is defined", () => {
+      expect(defaultTo("TestiArvo", "default")).to.equal("TestiArvo");
+    });
+    
+    it("Should return the value when number is defined", () => {
+      expect(defaultTo(8, "default")).to.equal(8);
+    });
+    
+    it("Should return the default value when empty list is given", () => {
+      const empty_list = [];
+      expect(defaultTo(empty_list, "default")).to.equal(empty_list);
+    });
+  });
 });
+
+/*
+defaultTo:
+Takes any value and any defaultValue as param
+Returns defaultValue if value = null/undefined/NaN
+otherwise returns value
+
+Tests for defaultTo:
+- Test that value is returned when its not undefined
+- Test that defaultValue is returned when value is undefined/null/NaN
+*/
