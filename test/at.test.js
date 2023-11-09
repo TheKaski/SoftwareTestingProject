5188b58fa78b947553d9f06c466c92273109dba8
TheKaski <emil.kaskikallio@gmail.com>
2023-11-09 16:45:41 +0200

    at tests written

--- test/at.test.js
+++ test/at.test.js
@@ -8,57 +8,62 @@ const object = { 'a': [{ 'b': { 'c': 3 } }, 4, null] }
 // THis test is copied from the example in the file at.js
 describe('Testing At from Software-testing-assignment', () => {
 
-  describe("Call with one path valid inputs", () => {
-    it("Should return the number value in array ", () => {
+  describe("Correct values should be found", () => {
+    it("Should return the correct value in array", () => {
       expect(at(object, ['a[0].b.c'])).to.deep.equal([3]);
     });
     it("Should return the null value in array ", () => {
       expect(at(object, ['a[2]'])).to.deep.equal([null]);
     });
-    it("Should return null when value is not found", () => {
-      expect(at(object, ['a[0].ö'])).to.deep.equal([undefined]);
-    });
-    it("Should return object in array ", () => {
+    it("Should return the value object in array", () => {
       expect(at(object, ['a[0]'])).to.deep.equal([{ 'b': { 'c': 3 } }]);
     });
-    it("Should return array in array ", () => {
+    it("Should return the value array in array", () => {
       expect(at(object, ['a'])).to.deep.equal([[{ 'b': { 'c': 3 } }, 4, null]]);
     });
+    it("Should return with empty list when path is empty", () => {
+      expect(at(object, [])).to.deep.equal([]);
+    });
   });
-  describe("Call with one path invalid inputs", () => {
-    it("Should return undefined in array when value is not found ", () => {
-      expect(at(object, ['b'])).to.deep.equal([undefined]);
-    });
-    it("Should return undefined when Index is out of bounds", () => {
-      expect(at(object, ['a[3]'])).to.deep.equal([undefined]);
-    });
-    it("Should return undefined when value is not found", () => {
+  describe("Values cannot be found but return should be handled correctly", () => {
+    it("Should return undefined when the value is not found", () => {
       expect(at(object, ['a[0].ö'])).to.deep.equal([undefined]);
     });
-    it("Should respond with undefined when object is null", () => {
-      expect(at(null, [])).to.deep.equal([]);
+    it("Should return undefined when the value index is out of bounds", () => {
+      expect(at(object, ['a[3]'])).to.deep.equal([undefined]);
+    });
+    it("Should return undefined when the value is not found", () => {
+      expect(at(object, ['a[0].ö'])).to.deep.equal([undefined]);
     });
    
+    it("Should respond with undefined in array", () => {
+      expect(at(object, undefined)).to.deep.equal([undefined]);
+    });
+    it("Should respond with undefined in array", () => {
+      expect(at(object, null)).to.deep.equal([undefined]);
+    });
+    it("Should respond with aray of undefined values", () => {
+      expect(at(object, [undefined, null])).to.deep.equal([undefined, undefined ]);
+    });
   });
 
   describe("Call with multiple valid paths", () => {
-    it("Should respond with correct values in array ", () => {
+    it("Should respond with  the correct values in array ", () => {
       expect(at(object, ['a[0].b.c', 'a[1]', 'a'])).to.deep.equal([3, 4, [{ 'b': { 'c': 3 } }, 4, null]]);
     });
   });
 
-  describe("CallS with invalid objects", () => {
-    it("Should respond with undefined when object is undefined", () => {
+  describe("Calls with unsearchable objects", () => {
+    it("Should return with array of undefined values when object is undefined", () => {
       expect(at(undefined, ['a[0].b.c', 'a[1]', 'a'])).to.deep.equal([undefined, undefined, undefined]);
     });
-    it("Should respond with undefined when object is null", () => {
+    it("Should return with array of undefined values when object is null", () => {
       expect(at(null, ['a[0].b.c', 'a[1]', 'a'])).to.deep.equal([undefined, undefined, undefined]);
     });
-
+    it("Should return with array of undefined values when object is empty", () => {
+      expect(at({}, ['a[0].b.c', 'a[1]', 'a'])).to.deep.equal([undefined, undefined, undefined]);
+    });
   });
-
-
-
 });
 
 /*
@@ -67,13 +72,14 @@ always return Array of values
 Is given object as parameter
 Is given a string or list of strings string[] to be picked from
 AT tests
-1. Call with only one path string
-2. Call with multiple paths string[]
-3. pick first level object
-4. pick second level object
-5. pick first level value
-6. pick second level value
-
+- Call with only one path string
+- Call with multiple paths string[]
+- pick first level objects
+- pick second level objects
+- pick first level values
+- pick second level values
+- Search with invalid and valid paths
+- Search with invalid and valid objects
 
 
 
