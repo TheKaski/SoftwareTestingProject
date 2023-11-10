ad45e78db21bbafc9f637bcc308913db3345211b
Emil Kaskikallio <80392752+TheKaski@users.noreply.github.com>
2023-11-10 13:53:21 +0200

    Local conflict file pushed accidentaly

--- test/defaultTo.test.sync-conflict-20231109-210835-V5CQ3CC.js
+++
@@ -1,9 +0,0 @@
-import {default as defaultTo} from "../src/defaultTo.js";
-import { assert } from 'chai';
-import { expect } from 'chai';
-import { should } from 'chai';
-
-
-describe("DefaultTo", () => {
- 
-});
